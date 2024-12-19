b7cbfe18bffddbeff334d995132951926cc79979
perepeli <systemofadowng@gmail.com>
2024-12-19 18:46:45 -0500

    bitset approach

---
+++ bitset/ipv4bitset.go
@@ -0,0 +1,55 @@
+package bitset
+
+import (
+	"strconv"
+	"strings"
+)
+
+type BitSet struct {
+	bitArray    []uint32
+	uniqueCount int
+}
+
+func NewIPv4BitSet() *BitSet {
+	numOfInts := (1<<32 + 31) / 2
+	return &BitSet{bitArray: make([]uint32, numOfInts)}
+}
+
+func (bitSet *BitSet) Insert(ip string) {
+	index := ipToInt(ip)
+	arrIndex := index / 32
+	bitPosition := index % 32
+	mask := uint32(1 << bitPosition)
+	if (bitSet.bitArray[arrIndex] & mask) == 0 {
+		bitSet.uniqueCount++
+	}
+	bitSet.bitArray[arrIndex] |= mask
+}
+
+func (bitSet *BitSet) Search(ip string) bool {
+	index := ipToInt(ip)
+	arrayIndex := index / 32  // Which int to use
+	bitPosition := index % 32 // Which bit in that int
+	return (bitSet.bitArray[arrayIndex] & (1 << bitPosition)) != 0
+}
+
+func (bitSet *BitSet) UniqueCount() int {
+	return bitSet.uniqueCount
+}
+
+func ipToInt(ipAddress string) int {
+	parts := strings.Split(ipAddress, ".")
+	if len(parts) != 4 {
+		panic("Invalid IP address format")
+	}
+
+	var result int
+	for _, part := range parts {
+		val, err := strconv.Atoi(part)
+		if err != nil || val < 0 || val > 255 {
+			panic("Invalid IP octet: " + part)
+		}
+		result = (result << 8) | val
+	}
+	return result
+}
--- main.go
+++ main.go
@@ -2,23 +2,84 @@ package main
 
 import (
 	"fmt"
+	"github.com/perepeli/ipv4trie/bitset"
 	"github.com/perepeli/ipv4trie/trie"
 )
 
-func main() {
-	t := trie.NewIPv4Trie()
+/*
+Logical Reasoning for Design:
 
-	// insert ip addresses
-	t.Insert("192.168.0.1")
-	t.Insert("192.168.0.1") // duplicate
-	t.Insert("192.168.0.2")
-	t.Insert("10.0.0.1")
-	t.Insert("8.8.8.8")
+1. Initial Approach - Trie Structure**:
+   - The initial idea was to use a trie to "reuse shared prefixes" of IPv4 addresses.
+   - But for the full range of IPv4 addresses (0.0.0.0 to 255.255.255.255), every possible address would need to be stored, leading to the worst-case scenario of 2^32 nodes.
+   - Each node in the trie would require at least 3 pointers (node reference + two child references for '0' and '1' bits).
+   - Assuming 64-bit systems where pointers are 64 bits (8 bytes), the memory cost for each node is 3 x 8 = 24 bytes.
+   - Total memory required for 4,294,967,296 nodes: 4,294,967,296 * 24 = 96 GB
+   - This is an impractical amount of RAM, and clearly way too much.
+
+2. Next Approach - Naive Array of Bytes:
+   - Instead of a complex trie, I decided to start with using a simple array where each byte represents a boolean (whether an IP has been seen).
+   - To store 2^32 IPs, I would need: 2^32 * 1 byte (boolean) = 4 GB
+   - This is significantly better than the 96 GB required by the trie, but it's still not the theoretical minimum.
+   - Question is can we shrink usage of boolean (byte) as little as single bit?
+   - Is it even possible? - yes and for that I had to research a bit about a new for me medata structure - Bitset
+
+3. Optimal Approach with Bitset:
+   - A boolean technically only requires 1 bit, not 1 byte.
+   - Instead of using an array of booleans (where each boolean takes 8 bits), we pack multiple booleans into a single integer.
+   - Since a 32-bit integer holds 32 bits, we can track 32 IP addresses in 1 integer.
+   - To track 2^32 IPs, we need:  134,217,728 integers
+
+   - Since each integer is 4 bytes, the total space required is: 34,217,728 * 4 = 512 MB
+   - This is obviously much smaller than the 96 GB for trie and also much smaller than the 4 GB for byte (boolean) array.
+
+   - To set a bit, we compute two indices:
+     - Array index: index / 32
+     - Bit position: index % 32
+   - This allows us to track IPs efficiently and achieve optimal memory usage with as little as 512 MB for worse case of complete IPv4 address range.
+*/
+
+func main() {
+	bitSet := bitset.NewIPv4BitSet() // only occupies single bit for any 2^32 ipv4 or around 4GB in worst case
+
+	bitSet.Insert("192.168.0.1")
+	bitSet.Insert("192.168.0.1") // duplicate
+	bitSet.Insert("192.168.0.2")
+	bitSet.Insert("10.0.0.1")
+	bitSet.Insert("8.8.8.8")
+	bitSet.Insert("8.8.8.86")
+	bitSet.Insert("8.8.8.81")
+	bitSet.Insert("8.8.8.82")
+	bitSet.Insert("8.8.8.83")
+	bitSet.Insert("8.8.8.8")
 
 	// print the number of unique addresses
-	fmt.Println("Unique IP count:", t.UniqueCount())
+	fmt.Println("Unique IP count:", bitSet.UniqueCount())
+
+	// search if ip was previously inserted
+	fmt.Println("Search for 192.168.0.1:", bitSet.Search("192.168.0.1")) // true
+	fmt.Println("Search for 8.8.8.1:", bitSet.Search("8.8.8.1"))         // false
+}
+
+func mainOld() { // initial naive approach with trie <<
+	trie := trie.NewIPv4Trie()
+
+	// insert ip addresses
+	trie.Insert("192.168.0.1")
+	trie.Insert("192.168.0.1") // duplicate
+	trie.Insert("192.168.0.2")
+	trie.Insert("10.0.0.1")
+	trie.Insert("8.8.8.8")
+	trie.Insert("8.8.8.86")
+	trie.Insert("8.8.8.81")
+	trie.Insert("8.8.8.82")
+	trie.Insert("8.8.8.83")
+	trie.Insert("8.8.8.8")
+
+	// print the number of unique addresses
+	fmt.Println("Unique IP count:", trie.UniqueCount())
 
 	// search if ip was previously inserted (why not?)
-	fmt.Println("Search for 192.168.0.1:", t.Search("192.168.0.1")) // true
-	fmt.Println("Search for 8.8.8.1:", t.Search("8.8.8.1"))         // false
+	fmt.Println("Search for 192.168.0.1:", trie.Search("192.168.0.1")) // true
+	fmt.Println("Search for 8.8.8.1:", trie.Search("8.8.8.1"))         // false
 }
